c1ce33aa2a8af534fd6999783205dcec6d2d6291
nijevise <calsoft@gmail.com>
2020-12-10 21:41:28 +0100

    Add project files.

---
+++ QA/BasicStats.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace QA
+{
+    class BasicStats
+    {
+        public static int GetLowest(List<int> RandomNumbers)
+        {
+            int LowestNumber = RandomNumbers.First();
+            foreach( int Number in RandomNumbers)
+            {
+                if(Number < LowestNumber)
+                {
+                    LowestNumber = Number;
+                }
+            }
+            return LowestNumber;
+        }
+
+
+        public static int GetHighest(List<int> RandomNumbers)
+        {
+            int HighestNumber = RandomNumbers.First();
+            foreach(int Number in RandomNumbers)
+            {
+                if(Number > HighestNumber)
+                {
+                    HighestNumber = Number;
+                }
+            }
+            return HighestNumber;
+        }
+
+        public static int GetSum(List<int> RandomNumbers)
+        {
+            int sum = 0;
+            foreach(int Number in RandomNumbers)
+            {
+                sum += Number;
+            }
+            return sum;
+
+        }
+        public static double GetAverage(List<int> RandomNumbers)
+        {
+            return GetSum(RandomNumbers) / RandomNumbers.Count();
+        }
+    }
+}
---
+++ QA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace QA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> RandomNumbers = new List<int>();
+            string UserInput;
+
+            do
+            {
+                Console.WriteLine("Unesite broj (ostavite prazan unos za kraj rada) >");
+                UserInput = Console.ReadLine();
+                if(UserInput != "")
+                {
+                    RandomNumbers.Add(Convert.ToInt32(UserInput));
+                }
+            } while (UserInput != "");
+            
+            Console.WriteLine("GetLowest = {0}", BasicStats.GetLowest(RandomNumbers));
+            Console.WriteLine("GetHighest = {0}", BasicStats.GetHighest(RandomNumbers));
+            Console.WriteLine("GetAverage = {0}", BasicStats.GetAverage(RandomNumbers));
+            Console.WriteLine("GetSum = {0}", BasicStats.GetSum(RandomNumbers));
+            
+            Console.ReadKey();
+        }
+    }
+}
